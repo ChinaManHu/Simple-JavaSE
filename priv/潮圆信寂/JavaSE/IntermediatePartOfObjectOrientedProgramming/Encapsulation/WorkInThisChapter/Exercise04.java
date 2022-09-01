13f9f5b3f3aa5213ca6e76fd973dc4f22964e0dd
rookie <95196780+ChinaManHu@users.noreply.github.com>
2022-09-01 14:33:27 +0800

    Add files via upload

---
+++ priv/潮圆信寂/JavaSE/IntermediatePartOfObjectOrientedProgramming/Encapsulation/WorkInThisChapter/Exercise04.java
@@ -0,0 +1,21 @@
+package priv.潮圆信寂.JavaSE.IntermediatePartOfObjectOrientedProgramming.Encapsulation.WorkInThisChapter;
+
+/**
+ *
+ *@author 潮圆信寂
+ *@date 2022/8/29
+ */
+public class Exercise04 {
+    public static void main(String[] args) {
+//        设计父类——员工类。子类:工人类,农民类,教师类,科学家类,服务生类。
+//        (1)其中工人,农民，服务生只有基本工资
+//        (2)教师除基本工资外，还有课酬(元/天)
+//        (3)科学家除基本工资外，还有年终奖
+//        (4)编写一个测试类,将各种类型的员工的全年工资打印出来
+
+    }
+}
+
+class NewEmployee {
+
+}
---
+++ priv/潮圆信寂/JavaSE/IntermediatePartOfObjectOrientedProgramming/Encapsulation/WorkInThisChapter/Exercise07.java
@@ -0,0 +1,87 @@
+package priv.潮圆信寂.JavaSE.IntermediatePartOfObjectOrientedProgramming.Encapsulation.WorkInThisChapter;
+
+/**
+ *
+ *@author 潮圆信寂
+ *@date 2022/8/29
+ */
+public class Exercise07 {
+    public static void main(String[] args) {
+//        编写Doctor类{name, age, job, gender, sal}相应的getter()和setter()方法，5个参数的构造器
+//        重写父类的equals(方法: public boolean equals(Object obj)
+//        并判断测试类中创建的两个对象是否相等,相等就是判断属性是否相同
+        Doctor doctor1 = new Doctor("Hqt1", 21, "医生", '男', 666666);
+        Doctor doctor2 = new Doctor("Hqt2", 21, "医生", '男', 666666);
+        System.out.println(doctor1.equals(doctor1));
+    }
+}
+
+class Doctor {
+    private String name;
+    private int age;
+    private String job;
+    private char gender;
+    private double sal;
+
+    public Doctor(String name, int age, String job, char gender, double sal) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.gender = gender;
+        this.sal = sal;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+        {
+            return this.name == ((Doctor)obj).name && this.job == ((Doctor)obj).job &&
+                    this.sal == ((Doctor)obj).sal && this.gender == ((Doctor)obj).gender &&
+                    this.age == ((Doctor)obj).age;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
+}
