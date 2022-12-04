fce7725a2d068660a6aa4c0720a9067763e49e67
rookie <95196780+ChinaManHu@users.noreply.github.com>
2022-12-04 16:49:35 +0800

    Add files via upload

---
+++ priv/潮圆信寂/JavaSE/IntermediatePartOfObjectOrientedProgramming/Encapsulation/InterFace/InterFaceTest/DatabaseInterFace.java
@@ -0,0 +1,6 @@
+package priv.潮圆信寂.JavaSE.IntermediatePartOfObjectOrientedProgramming.Encapsulation.InterFace.InterFaceTest;
+
+public interface DatabaseInterFace {
+    public void connect();
+    public void close();
+}
