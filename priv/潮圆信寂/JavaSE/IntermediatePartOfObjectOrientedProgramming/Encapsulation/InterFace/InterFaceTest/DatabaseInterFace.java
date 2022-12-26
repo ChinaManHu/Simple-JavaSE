7973e38ada0a1fada92a54121437c83f4d71fff7
rookie <95196780+ChinaManHu@users.noreply.github.com>
2022-12-26 18:55:34 +0800

    Delete priv/潮圆信寂/JavaSE/IntermediatePartOfObjectOrientedProgramming/Encapsulation directory

--- priv/潮圆信寂/JavaSE/IntermediatePartOfObjectOrientedProgramming/Encapsulation/InterFace/InterFaceTest/DatabaseInterFace.java
+++
@@ -1,6 +0,0 @@
-package priv.潮圆信寂.JavaSE.IntermediatePartOfObjectOrientedProgramming.Encapsulation.InterFace.InterFaceTest;
-
-public interface DatabaseInterFace {
-    public void connect();
-    public void close();
-}
