bc3f7d2bb218692142fffbd991abcf09fb6550d2
rookie <95196780+ChinaManHu@users.noreply.github.com>
2023-01-04 15:50:42 +0800

    Add files via upload

---
+++ priv/潮圆信寂/JavaSE/AdvancedPart/WorkInThisChapter/Exercise06.java
@@ -0,0 +1,18 @@
+package priv.潮圆信寂.JavaSE.AdvancedPart.WorkInThisChapter;
+
+/**
+ *
+ * @author 潮圆信寂
+ * @date 2022/12/29 20:39
+ *
+ */
+
+/*
+内部类练习
+有一个Car类，有属性temperature (温度) ,车内有Air (空调)类，有吹风的功能flow,
+Air会监视车内的温度，如果温度超过40度则吹冷气。如果温度低于0度则吹暖气，如果在这之
+间则关掉空调。实例化具有不同温度的Car对象，调用空调的flow方法，测试空调吹的风是否正确.
+ */
+
+public class Exercise06 {
+}
